cdf744a3ade88f5196f3ba886cb4b783f0fb8838
davithekid <byjdrk.27@gmail.com>
2025-08-28 10:58:17 -0300

    fix: respons

--- src/components/blocks/navbar/Navbar.jsx
+++ src/components/blocks/navbar/Navbar.jsx
@@ -8,14 +8,14 @@ export default function Navbar() {
             <nav>
                 <div className="mx-auto flex px-10 py-5 bg-white justify-between items-center z-100">
                     <ul className="flex items-center gap-12 dark:text-black">
-                        <li className="font-bold text-xl"><a href="">Logo</a></li>
+                        <li className="font-bold text-xl"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Grammarly_logo.svg/1200px-Grammarly_logo.svg.png" alt="" className="w-40" /></li>
                         <li className=" "><NavigationMenuDemo/></li>
                     </ul>
 
                     <div className="dark:text-black flex items-center">
                         <ModeToggle/>   
                         <Button className="cursor-pointer w-40 ml-5 mr-5 font-bold bg-white hover:bg-zinc-200 duration-200 text-black  ">My Grammarly</Button>
-                        <Button className="cursor-pointer w-40 bg-green-700 font-bold hover:bg-green-800/70 text-white">Start a Free Trial</Button>
+                        <Button className="cursor-pointer w-40 bg-emerald-700 font-bold hover:bg-green-800/70 text-white">Start a Free Trial</Button>
                     </div>
                 </div>  
             </nav>
--- src/components/sections/index.jsx
+++ src/components/sections/index.jsx
@@ -7,13 +7,13 @@ export default function Principal() {
                 <div className="container mx-auto py-5 lg:py-0 lg:ml-30 content-center justify-items-center">
                     <div className="p-5">
                         <h1 className="text-4xl md:text-5x1 lg:text-5xl font-extrabold lg:w-130 text-center lg:text-left">Al Your Business Can Run With</h1>
-                        <p className="text-lg md:text-xl mt-5 lg:mt-10 xl:text-xl lg:w-140 font-semibold">Grammarly Business combines advanced AI communication assistance with the collective knowledge and style of your organization to help teams communicate more effectively. It provides real-time writing suggestions </p>
+                        <p className="text-lg md:text-xl mt-5 lg:mt-10 xl:text-xl lg:w-120 font-semibold">Grammarly Business combines advanced AI communication assistance with the collective knowledge and style of your organization to help teams communicate more effectively. It provides real-time writing suggestions </p>
                         <Button className="cursor-pointer mx-auto bg-green-800 hover:bg-green-800/80 dark:bg-zinc-200  mt-10  md:h-15 md:w-50 font-bold md:text-lg">Start a Free Trial</Button>
                     </div>
                 </div>
 
                 <div className="content-center">
-                    <img src="./teste.webp" alt="" />
+                    <img src="./banner.svg" alt="" className="w-180" />
                 </div>
             </div>
         </>
